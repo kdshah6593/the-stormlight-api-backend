13549a7a7c7b9129b2b9bac31f6cc38430113fe6
Kunal <kdshah6593@gmail.com>
2021-04-26 18:18:12 -0400

    reset db, add rows to books table

--- db/migrate/20210426213113_create_books.rb
+++ db/migrate/20210426213113_create_books.rb
@@ -2,9 +2,13 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
+      t.string :author
       t.string :url
       t.string :isbn
-      t.integer :pages
+      t.integer :hardcover_pages
+      t.integer :paperback_pages
+      t.integer :chapters
+      t.string :book_type
       t.string :publisher
       t.integer :year_released
 
