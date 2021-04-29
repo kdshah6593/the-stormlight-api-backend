a5db05c21e84cb460b76292a87a2f62a4f16d858
Kunal <kdshah6593@gmail.com>
2021-04-29 17:07:51 -0400

    routes for locations and races

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :books, only: [:index, :show, :edit, :update]
+      resources :locations, only: [:index, :show, :edit, :update]
+      resources :races, only: [:index, :show, :edit, :update]
     end
   end
 end
