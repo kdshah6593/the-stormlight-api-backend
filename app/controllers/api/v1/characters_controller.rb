d5a52e1279202259aa6ede9d99c123ac2b0292c6
Kunal <kdshah6593@gmail.com>
2021-05-04 16:53:26 -0400

    controller actions

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::CharactersController < ApplicationController
+
+    def index
+        @characters = Character.all
+        render json: @characters
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
 end
