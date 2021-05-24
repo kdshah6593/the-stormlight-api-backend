f7dd03f1bb746a4c8def56c79ecf9965b02983bf
Kunal <kdshah6593@gmail.com>
2021-05-24 17:16:29 -0400

    set up race character association, character table columns

--- db/migrate/20210504204943_create_characters.rb
+++ db/migrate/20210504204943_create_characters.rb
@@ -1,8 +1,26 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     create_table :characters do |t|
+      t.string :name
+      t.string :gender
+      t.boolean :alive
+      t.string :titles, array: true, default:[]
+      t.string :aliases, array: true, default:[]
+      t.string :father
+      t.string :mother
+      t.string :spouse
+      t.string :occupation
+      t.string :affiliations, array: true, default:[]
 
       t.timestamps
     end
   end
 end
+
+
+# Ethnicity/Race, Nationality/Nation, born, died, groups Knights Radiant Order, Shardbearer (true/false) books (associated), point of view books (associated)
+# Race is associations
+# figure out born/died with datetime type
+# for knights radiant, maybe make boolean and then another column is dependent on that boolean being true or false
+# associated character with books
+# associated point of view books
