6fa5eb1e1a1ea9d34ffb01a1a7173263e9caafbe
Kunal <kdshah6593@gmail.com>
2021-10-18 18:20:20 -0400

    add method for generate jwt token; added comments for validations to use later

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,17 @@ class User < ApplicationRecord
   enum role: [:basic, :verified, :admin]
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
-  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
-  after_initialize do
-    if self.new_record?
-      self.role ||= :basic
-    end
+  # validates :username, presence: true
+  # validates :email, presence: true
+  # validates :password, presence: true
+  # validates :password, length: { in: 6..20 }, on: :create
+  # validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  # validates :email, uniqueness: true
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'requires a valid email format'}
+
+  def generate_jwt
+    JWT.encode({id: id, exp: 60.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
   end
+
 end
